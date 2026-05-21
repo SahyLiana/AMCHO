5420cc6ce525d3d9de1a65d0334b4a5178fd4704
Harison <harison.rhj@gmail.com>
2026-05-22 00:35:27 +0300

    correcting login default route

--- amcho-frontend/src/App.jsx
+++ amcho-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 // import { useState, useEffect } from "react";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -12,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route
           path="/login"
           element={<LoginPage user={user} setUser={setUser} />}
